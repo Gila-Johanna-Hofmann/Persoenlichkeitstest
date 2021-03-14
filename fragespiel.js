48d56da57d77f439c0c7de20436785210ce9aea0
Gila.Hofmann <Gila.Hofmann@HN-GILA>
2021-03-14 16:03:55 +0100

    added comments

--- fragespiel.js
+++ fragespiel.js
@@ -1,3 +1,5 @@
+/*DOMs*/
+
 const startseite = document.getElementById("startseite"); //erzeugt ein Element aus den html-Elementen
 
 const startbutton = document.getElementById("startbutton");
@@ -10,6 +12,7 @@ const auswertenButton = document.getElementById("auswertenButton");
 
 const goAgainButton = document.getElementById("goAgainButton");
 
+/*changing visibility of sections via click of startbutton*/
 function startbuttonClick() {
     startseite.classList.add("invisible");
     quizteil.classList.remove("invisible");
@@ -17,6 +20,10 @@ function startbuttonClick() {
 
 startbutton.addEventListener("click", startbuttonClick);
 
+/*counting the results from the radio buttons. very simple.
+the outcome has just two options ("heinz" and "horst"), which
+are assigned the values "a" and "b". the default outcome is always
+"horst"/"b"*/
 function evaluateResults() {
     let radios = document.querySelectorAll('input[type="radio"]:checked');
 
@@ -38,6 +45,10 @@ function evaluateResults() {
 
     console.log(heinz, horst);
 
+    /*for learning purposes and because the picture and text displayed
+    depend on the outcome: instead of linking the picture in the 
+    html as with the #startseite section, I decided to link the 
+    picture for the #auswertung section with JS, using the #ergebnis id*/
     const picture = document.createElement("img");
 
     if (heinz > horst) {
@@ -54,6 +65,8 @@ function evaluateResults() {
 
 }
 
+/*thie button click takes the site back to the original settings.
+the quiz can be taken again*/
 function submitButtonClick(event) {
     event.preventDefault();
     quizteil.classList.add("invisible");
@@ -64,10 +77,13 @@ function submitButtonClick(event) {
 
 auswertenButton.addEventListener("click", submitButtonClick);
 
-goAgainButton.addEventListener("click", function () { //das ist eine anonyme Funktion!!!!!!!!!
+goAgainButton.addEventListener("click", function () { /* <--- das ist 
+    eine anonyme Funktion!!!!!!!!!*/
     location.reload();
 })
 
-// startbuttonClick(); //nachher wieder löschen ;_)
+/*when working on the #quizteil section, the following can be 
+uncommented, so the sites always dispplayes the #quizteil section*/
+// startbuttonClick(); 
 
 
